e5efe79d8d016eb408ee5cbe3335c4fb517055e6
cho0op <sorintema@gmail.com>
2023-05-21 11:34:46 +0300

    Fixed controllers

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -4,7 +4,7 @@ import { ReviewModel } from './review.model';
 @Controller('review')
 export class ReviewController {
   @Post('create')
-  async create(@Body dto: Omit<ReviewModel, '_id'>) {}
+  async create(@Body() dto: Omit<ReviewModel, '_id'>) {}
 
   @Delete(':id')
   async delete(@Param('id') id: string) {}
