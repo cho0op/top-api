7120c97255b538ce6477813afab6f3c1c2498e28
cho0op <sorintema@gmail.com>
2023-05-26 10:55:57 +0300

    Added login service and controller

--- src/auth/auth.contants.ts
+++ src/auth/auth.contants.ts
@@ -1,1 +1,3 @@
-export const ALREADY_REGISTERED = 'User already exists';
+export const USER_ALREADY_REGISTERED = 'User already exists';
+export const USER_NOT_FOUND = 'User not found';
+export const INVALID_LOGIN_DATA = 'Wrong password or email';
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,7 +7,7 @@ import {
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
-import { ALREADY_REGISTERED } from './auth.contants';
+import { USER_ALREADY_REGISTERED } from './auth.contants';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 
@@ -20,13 +20,18 @@ export class AuthController {
   async register(@Body() dto: AuthDto) {
     const oldUser = await this.authService.findUser(dto.login);
     if (oldUser) {
-      throw new BadRequestException(ALREADY_REGISTERED);
+      throw new BadRequestException(USER_ALREADY_REGISTERED);
     }
 
     return this.authService.create(dto);
   }
 
+  @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('login')
-  async login(@Body() dto: AuthDto) {}
+  async login(@Body() dto: AuthDto) {
+    const user = await this.authService.validateUser(dto.login, dto.password);
+
+    return this.authService.login(user.email);
+  }
 }
