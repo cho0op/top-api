afcc24c9c4adaa900f01ea032c700d326c249798
cho0op <sorintema@gmail.com>
2023-05-26 10:14:16 +0300

    Added register controller and service

---
+++ src/auth/auth.contants.ts
@@ -0,0 +1,1 @@
+export const ALREADY_REGISTERED = 'User already exists';
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,31 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { ALREADY_REGISTERED } from './auth.contants';
+import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @UsePipes(new ValidationPipe())
   @Post('register')
-  async register(@Body() dto: AuthDto) {}
+  async register(@Body() dto: AuthDto) {
+    const oldUser = await this.authService.findUser(dto.login);
+    if (oldUser) {
+      throw new BadRequestException(ALREADY_REGISTERED);
+    }
+
+    return this.authService.create(dto);
+  }
+
   @HttpCode(200)
   @Post('login')
   async login(@Body() dto: AuthDto) {}
