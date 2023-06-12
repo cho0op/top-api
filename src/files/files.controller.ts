fb1273d1af04fa50cf3dce0ca820ed7c03eff3a4
cho0op <sorintema@gmail.com>
2023-06-12 16:24:34 +0300

    Added file upload endpoint with validation

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,16 +1,34 @@
 import {
   Controller,
   HttpCode,
+  HttpStatus,
+  ParseFilePipeBuilder,
   Post,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { FileElementResponse } from './dto/file-element.response';
+import { FilesService } from './files.service';
 
 @Controller('files')
 export class FilesController {
+  constructor(private readonly filesService: FilesService) {}
+
   @Post('upload')
   @HttpCode(200)
-  @UseInterceptors(FileInterceptor('files'))
-  async uploadFile(@UploadedFile() file: Express.Multer.File) {}
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(
+    @UploadedFile(
+      new ParseFilePipeBuilder().build({
+        errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      }),
+    )
+    file: Express.Multer.File,
+  ): Promise<FileElementResponse[]> {
+    return this.filesService.saveFiles([file]);
+  }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { path } from 'app-root-path';
+import { format } from 'date-fns';
+import { ensureDir, writeFile } from 'fs-extra';
+import { FileElementResponse } from './dto/file-element.response';
 
 @Injectable()
-export class FilesService {}
+export class FilesService {
+  async saveFiles(
+    files: Express.Multer.File[],
+  ): Promise<FileElementResponse[]> {
+    const dateFolder = format(new Date(), 'yyyy-MM-dd');
+    const uploadedPath = `${path}/uploads/${dateFolder}`;
+    await ensureDir(uploadedPath);
+    const response: FileElementResponse[] = [];
+    for (const file of files) {
+      const newFilePath = `${uploadedPath}/${file.originalname}`;
+      await writeFile(newFilePath, file.buffer);
+      response.push({ url: newFilePath, name: file.originalname });
+    }
+
+    return response;
+  }
+}
