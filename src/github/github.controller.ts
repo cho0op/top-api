836c4249e0688ab77e140bf3a0b2514b0189824d
cho0op <sorintema@gmail.com>
2023-06-15 20:32:32 +0300

    Added github.controller.ts for tests

---
+++ src/github/github.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { GithubService } from './github.service';
+
+@Controller('github')
+export class GithubController {
+  constructor(private readonly githubService: GithubService) {}
+
+  @Get('data') async getData() {
+    this.githubService.getData();
+  }
+}
